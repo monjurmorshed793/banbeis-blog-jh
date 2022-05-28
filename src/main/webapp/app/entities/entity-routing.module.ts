bbab27fd3a852621447f91f3dd0c262247583b28
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-05-28 21:33:14 +0600

    Rolled back to 7.8.0 due to angular specific bugs.

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,11 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'navigation',
-        data: { pageTitle: 'banbeisBlogApp.navigation.home.title' },
-        loadChildren: () => import('./navigation/navigation.module').then(m => m.NavigationModule),
-      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
